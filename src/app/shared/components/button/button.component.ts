a3b4e1f0be5cfb78949bd5db7d2c71cc2a5982df
koksoni <47037656+koksoni@users.noreply.github.com>
2023-04-22 23:34:21 +0400

    website-footer

--- src/app/shared/components/button/button.component.ts
+++ src/app/shared/components/button/button.component.ts
@@ -3,5 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-button',
   template: 'todo button',
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.scss']
 })
 export class ButtonComponent {}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 
+
 @NgModule({
   declarations: [ButtonComponent, HeaderComponent, FooterComponent],
   imports: [CommonModule],
