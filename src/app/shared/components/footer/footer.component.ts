af0010f2a61c648699ece04d764bafc02cc7094d
koksoni <47037656+koksoni@users.noreply.github.com>
2023-04-22 23:34:21 +0400

    website-footer

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
-  template: 'FOOTER',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent {}
