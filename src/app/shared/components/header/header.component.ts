d7c334ddc19fe01e7f08caaedfddf32fe54ddb54
tengo7777 <108832273+tengo7777@users.noreply.github.com>
2023-04-25 06:55:33 -0700

    header-finished

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit, AfterViewInit {
+  dropdownPopoverShow = false;
+  showNavbar: boolean = false;
+  showMobileMenu: boolean = false;
+  showIndusrtyMenu: boolean = false;
+  constructor() {
+  }
+  ngOnInit(): void {
+  }
+  @ViewChild("btnDropdownRef", { static: false }) btnDropdownRef!: ElementRef;
+  @ViewChild("popoverDropdownRef", { static: false })
+  popoverDropdownRef!: ElementRef;
+
+  ngAfterViewInit() {
+    createPopper(
+      this.btnDropdownRef.nativeElement,
+      this.popoverDropdownRef.nativeElement,
+      {
+        placement: "bottom-start",
+      }
+    );
+  }
+  toggleDropdown(event: any) {
+    event.preventDefault();
+    if (this.dropdownPopoverShow) {
+      this.dropdownPopoverShow = false;
+    } else {
+      this.dropdownPopoverShow = true;
+    }
+  }
 
 }
+
+function createPopper(nativeElement: any, nativeElement1: any, arg2: { placement: string; }) {
+  throw new Error('Function not implemented.');
+}
+
+
