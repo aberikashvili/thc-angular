f0340394216e7206d2f59406495a36ee8725d3ff
tengo7777 <108832273+tengo7777@users.noreply.github.com>
2023-04-25 06:55:33 -0700

    header-finished

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
-  template: 'FOOTER',
-  
+  template: '',
 })
-export class FooterComponent {}
+export class FooterComponent { }
