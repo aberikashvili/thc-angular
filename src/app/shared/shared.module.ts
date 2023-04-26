e54dbb836ad9cb0b32673dc3b6a89625af304c8d
koksoni <47037656+koksoni@users.noreply.github.com>
2023-04-26 21:16:16 +0400

    button

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,11 +3,10 @@ import { ButtonComponent } from './components/button/button.component';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { OrangeButtonComponent } from './components/button/orange-button/orange-button.component';
-import { BlueButtonComponent } from './components/button/blue-button/blue-button.component';
+
 
 @NgModule({
-  declarations: [ButtonComponent, HeaderComponent, FooterComponent,  OrangeButtonComponent, BlueButtonComponent],
+  declarations: [ButtonComponent, HeaderComponent, FooterComponent ],
   imports: [CommonModule],
   exports: [CommonModule, ButtonComponent, HeaderComponent, FooterComponent],
 })
