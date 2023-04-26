d7a8917fb596ca63b0b4ac8175ea41a9ac0b3c30
koksoni <47037656+koksoni@users.noreply.github.com>
2023-04-26 21:16:16 +0400

    button

--- src/app/shared/components/button/button.component.ts
+++ src/app/shared/components/button/button.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { Component, EventEmitter,Input,Output } from '@angular/core';
+export type ButtonType = 'orange' | 'blue'
 
 @Component({
   selector: 'app-button',
@@ -6,4 +8,24 @@ import { Component } from '@angular/core';
   templateUrl: './button.component.html',
   styleUrls: ['./button.component.scss']
 })
-export class ButtonComponent {}
+export class ButtonComponent {
+
+ 
+  @Input() type:ButtonType='orange'
+
+  @Output() clicked= new EventEmitter();
+ 
+
+
+  get orange():boolean{
+    return this.type==="orange"
+  }
+  get blue():boolean{
+    return this.type==="blue"
+  }
+  btnClicked(){
+    this.clicked.emit()
+  }
+
+
+}
