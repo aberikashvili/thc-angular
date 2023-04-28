0c42fc1add890415e30b8512fe89fd94bf80e3ed
koksoni <47037656+koksoni@users.noreply.github.com>
2023-04-28 21:30:55 +0400

    .

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,9 +3,10 @@ import { ButtonComponent } from './components/button/button.component';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { StatisticsComponent } from './components/statistics/statistics.component';
 
 @NgModule({
-  declarations: [ButtonComponent, HeaderComponent, FooterComponent],
+  declarations: [ButtonComponent, HeaderComponent, FooterComponent, StatisticsComponent],
   imports: [CommonModule],
   exports: [CommonModule, ButtonComponent, HeaderComponent, FooterComponent],
 })
