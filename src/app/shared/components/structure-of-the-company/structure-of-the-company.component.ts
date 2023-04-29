19e9fe04a71dc875ea0a6870dfa0c4555f213b49
koksoni <47037656+koksoni@users.noreply.github.com>
2023-04-30 01:07:18 +0400

    structure-of-the-company-section-done

---
+++ src/app/shared/components/structure-of-the-company/structure-of-the-company.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-structure-of-the-company',
+  templateUrl: './structure-of-the-company.component.html',
+  styleUrls: ['./structure-of-the-company.component.scss']
+})
+export class StructureOfTheCompanyComponent {
+  
+
+}
