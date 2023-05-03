86a31d4be716cbabb632eb3ae13bd9a3beb0c80b
tengo7777 <108832273+tengo7777@users.noreply.github.com>
2023-05-03 07:33:01 -0700

    home-modul progress

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
   dropdownPopoverShow = false;
@@ -11,10 +11,5 @@ export class HeaderComponent {
   showMobileMenu: boolean = false;
   showIndusrtyMenu: boolean = false;
 
-  onbuttonclick() {
-
-  }
+  onbuttonclick() {}
 }
-
-
-
