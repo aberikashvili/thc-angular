c8d91494d0fdbb808726b4b5a07d4d4bd0cb4a30
koksoni <47037656+koksoni@users.noreply.github.com>
2023-05-07 00:07:08 +0400

    13-who-we-are-ის-structure-of-the-company-section almost-done

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,11 +4,12 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HomePageComponent } from './components/home-page/home-page.component';
+import { WhoWeAreComponent } from './components/who-we-are/who-we-are.component';
 
 
 @NgModule({
-  declarations: [ButtonComponent, HeaderComponent, FooterComponent, HomePageComponent],
+  declarations: [ButtonComponent, HeaderComponent, FooterComponent, HomePageComponent, WhoWeAreComponent],
   imports: [CommonModule],
-  exports: [CommonModule, ButtonComponent, HeaderComponent, FooterComponent, HomePageComponent],
+  exports: [CommonModule, ButtonComponent, HeaderComponent, FooterComponent, HomePageComponent,WhoWeAreComponent],
 })
 export class SharedModule { }
