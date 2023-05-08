88d499ba49d854edab17ccfab85bf41e0d99c5dc
koksoni <47037656+koksoni@users.noreply.github.com>
2023-05-09 00:57:08 +0400

    feature/who-we-are-Structure-of-the-Company-ready-for-testing

---
+++ src/app/modules/who-we-are/components/who-we-are-structure/who-we-are-structure.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-who-we-are-structure',
+  templateUrl: './who-we-are-structure.component.html',
+  styleUrls: ['./who-we-are-structure.component.scss']
+})
+export class WHOWEAREStructureComponent {
+
+}
--- src/app/modules/who-we-are/who-we-are.module.ts
+++ src/app/modules/who-we-are/who-we-are.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { WhoWeAreComponent } from './who-we-are.component';
+import { WHOWEAREStructureComponent } from './components/who-we-are-structure/who-we-are-structure.component';
 
 const routes: Routes = [
   {
@@ -12,7 +13,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  declarations: [WhoWeAreComponent],
+  declarations: [WhoWeAreComponent, WHOWEAREStructureComponent],
   imports: [RouterModule.forChild(routes), SharedModule],
 })
 export class WhoWeAreModule {}
