73569872012a166e4fab7aece1f753bdf0ed943c
Arkadi Berikashvili <superhornet111@gmail.com>
2023-05-08 19:15:23 +0400

    Revert "Feature/add home module"

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,12 +3,11 @@ import { ButtonComponent } from './components/button/button.component';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { HomePageComponent } from './components/home-page/home-page.component';
 
 
 @NgModule({
-  declarations: [ButtonComponent, HeaderComponent, FooterComponent, HomePageComponent],
+  declarations: [ButtonComponent, HeaderComponent, FooterComponent],
   imports: [CommonModule],
-  exports: [CommonModule, ButtonComponent, HeaderComponent, FooterComponent, HomePageComponent],
+  exports: [CommonModule, ButtonComponent, HeaderComponent, FooterComponent],
 })
 export class SharedModule { }
