204c0b0449d2b3c376a1d2582a03c00bcbbc45a6
ucubabu <ucunashviligio@gmail.com>
2023-05-09 19:15:09 +0400

    started

---
+++ src/app/modules/who-we-are/whowearesection/whowearesection.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-whowearesection',
+  templateUrl: './whowearesection.component.html',
+  styleUrls: ['./whowearesection.component.scss']
+})
+export class WhowearesectionComponent {
+
+}
