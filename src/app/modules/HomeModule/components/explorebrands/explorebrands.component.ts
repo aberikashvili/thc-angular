3f2f08c52863500fb4ed4a5df29f3366e3263791
ucubabu <ucunashviligio@gmail.com>
2023-05-12 20:08:04 +0400

    dad

--- src/app/modules/HomeModule/components/explorebrands/explorebrands.component.ts
+++ src/app/modules/HomeModule/components/explorebrands/explorebrands.component.ts
@@ -6,5 +6,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./explorebrands.component.scss']
 })
 export class ExplorebrandsComponent {
+tabsData: any = [];
+  filteredCategoryData: any = [];
 
+  ngOnInit(): void {
+    this.getCategoryByType();
+  }
+
+
+
+  getCategoryByType(type: string = 'all') {
+    let categoryData = [
+      { img: '/assets/images/Group.png', type: '1', detailUrl: 'Details' },
+      { img: '/assets/images/Group.png', type: '1', detailUrl: false },
+      { img: '/assets/images/Group.png', type: '1', detailUrl: 'Details' },
+      { img: '/assets/images/Group.png', type: '2', detailUrl: false },
+      { img: '/assets/images/Group.png', type: '2', detailUrl: false },
+      { img: '/assets/images/Group.png', type: '2', detailUrl: false },
+      { img: '/assets/images/Group.png', type: '3', detailUrl: false },
+      { img: '/assets/images/Group.png', type: '3', detailUrl: false },
+      { img: '/assets/images/Group.png', type: '3', detailUrl: false },
+      { img: '/assets/images/Group.png', type: '4', detailUrl: 'Details' },
+      { img: '/assets/images/Group.png', type: '4', detailUrl: false },
+      { img: '/assets/images/Group.png', type: '4', detailUrl: false },
+      { img: '/assets/images/Group.png', type: '5', detailUrl: false },
+      { img: '/assets/images/Group.png', type: '5', detailUrl: false },
+      { img: '/assets/images/Group.png', type: '5', detailUrl: false },
+      { img: '/assets/images/Group.png', type: '5', detailUrl: false },
+      { img: '/assets/images/Group.png', type: '5', detailUrl: false },
+      { img: '/assets/images/Group.png', type: '5', detailUrl: false },
+    ];
+}
 }
