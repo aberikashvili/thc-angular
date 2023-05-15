563c38cec057d0c111c3a5a406c4ce11b1ecc19e
ucubabu <ucunashviligio@gmail.com>
2023-05-15 13:40:27 +0400

    save

--- src/app/modules/HomeModule/components/explorebrands/explorebrands.component.ts
+++ src/app/modules/HomeModule/components/explorebrands/explorebrands.component.ts
@@ -3,37 +3,34 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-explorebrands',
   templateUrl: './explorebrands.component.html',
-  styleUrls: ['./explorebrands.component.scss']
+  styleUrls: ['./explorebrands.component.scss'],
 })
 export class ExplorebrandsComponent {
-filteredCategoryData: any = [];
+  categoryData: any = [
+    { img: '/assets/images/Group.png', type: '1', detailUrl: false },
+    { img: '/assets/images/Group.png', type: '1', detailUrl: false },
+    { img: '/assets/images/Group.png', type: '1', detailUrl: false },
+    { img: '/assets/images/Group.png', type: '2', detailUrl: 'Details' },
+    { img: '/assets/images/Group.png', type: '2', detailUrl: false },
+    { img: '/assets/images/Group.png', type: '2', detailUrl: false },
+    { img: '/assets/images/Group.png', type: '3', detailUrl: false },
+    { img: '/assets/images/Group.png', type: '2', detailUrl: false },
+  ];
 
-  ngOnInit(): void {
-    this.getCategoryByType();
-  }
+  // ngOnInit(): void {
+  //   this.getCategoryByType();
+  // }
 
-
-
-  getCategoryByType(type: string = 'all') {
-    let categoryData = [
-      { img: '/assets/images/Group.png', type: '1', detailUrl: 'Details' },
-      { img: '/assets/images/Group.png', type: '1', detailUrl: false },
-      { img: '/assets/images/Group.png', type: '1', detailUrl: 'Details' },
-      { img: '/assets/images/Group.png', type: '2', detailUrl: false },
-      { img: '/assets/images/Group.png', type: '2', detailUrl: false },
-      { img: '/assets/images/Group.png', type: '2', detailUrl: false },
-      { img: '/assets/images/Group.png', type: '3', detailUrl: false },
-      { img: '/assets/images/Group.png', type: '3', detailUrl: false },
-      { img: '/assets/images/Group.png', type: '3', detailUrl: false },
-      { img: '/assets/images/Group.png', type: '4', detailUrl: 'Details' },
-      { img: '/assets/images/Group.png', type: '4', detailUrl: false },
-      { img: '/assets/images/Group.png', type: '4', detailUrl: false },
-      { img: '/assets/images/Group.png', type: '5', detailUrl: false },
-      { img: '/assets/images/Group.png', type: '5', detailUrl: false },
-      { img: '/assets/images/Group.png', type: '5', detailUrl: false },
-      { img: '/assets/images/Group.png', type: '5', detailUrl: false },
-      { img: '/assets/images/Group.png', type: '5', detailUrl: false },
-      { img: '/assets/images/Group.png', type: '5', detailUrl: false },
-    ];
-}
+  // getCategoryByType() {
+  //   let categoryData = [
+  //     { img: '/assets/images/Group.png', type: '1', detailUrl: false },
+  //     { img: '/assets/images/Group.png', type: '1', detailUrl: false },
+  //     { img: '/assets/images/Group.png', type: '1', detailUrl: false },
+  //     { img: '/assets/images/Group.png', type: '2', detailUrl: 'Details' },
+  //     { img: '/assets/images/Group.png', type: '2', detailUrl: false },
+  //     { img: '/assets/images/Group.png', type: '2', detailUrl: false },
+  //     { img: '/assets/images/Group.png', type: '3', detailUrl: false },
+  //     { img: '/assets/images/Group.png', type: '2', detailUrl: false },
+  //   ];
+  // }
 }
