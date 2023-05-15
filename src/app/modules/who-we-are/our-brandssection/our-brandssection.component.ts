630b69c36a699242d071ab328613b2e1731acb17
ucubabu <ucunashviligio@gmail.com>
2023-05-15 13:08:21 +0400

    for mobile

---
+++ src/app/modules/who-we-are/our-brandssection/our-brandssection.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-our-brandssection',
+  templateUrl: './our-brandssection.component.html',
+  styleUrls: ['./our-brandssection.component.scss']
+})
+export class OurBrandssectionComponent {
+
+}
--- src/app/modules/who-we-are/who-we-are.module.ts
+++ src/app/modules/who-we-are/who-we-are.module.ts
@@ -6,6 +6,9 @@ import { WHOWEAREStructureComponent } from './components/who-we-are-structure/wh
 import { WhoWeAreMediaComponent } from './components/who-we-are-media/who-we-are-media.component';
 import { WhoWeAreHeroSectionComponent } from './components/who-we-are-hero-section/who-we-are-hero-section.component';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { WhoWeAreComponent } from './who-we-are.component';
+import { OurBrandssectionComponent } from './our-brandssection/our-brandssection.component';
 
 const routes: Routes = [
   {
@@ -20,6 +23,7 @@ const routes: Routes = [
     WHOWEAREStructureComponent,
     WhoWeAreMediaComponent,
     WhoWeAreHeroSectionComponent,
+    OurBrandssectionComponent,
   ],
   imports: [RouterModule.forChild(routes), SharedModule],
 })
