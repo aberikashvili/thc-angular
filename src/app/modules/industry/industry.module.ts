5690542226a15020307a023a8d3fd82b5a303844
koksoni <47037656+koksoni@users.noreply.github.com>
2023-05-17 02:07:53 +0400

    .

--- src/app/modules/industry/industry.module.ts
+++ src/app/modules/industry/industry.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IndustryComponent } from './industry.component';
-import { IndustryBrandsGalleryComponent } from './components/industry-brands-gallery/industry-brands-gallery.component';
 import { RouterModule, Routes } from '@angular/router';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { IndustryGalleryComponent } from './components/industry-gallery/industry-gallery.component';
+import { IndustryBrandsComponent } from './components/industry-brands/industry-brands.component';
 
 const routes: Routes = [
   {
@@ -14,7 +14,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  declarations: [IndustryComponent,IndustryBrandsGalleryComponent, IndustryGalleryComponent, ],
+  declarations: [IndustryComponent, IndustryGalleryComponent, IndustryBrandsComponent, ],
   imports: [RouterModule.forChild(routes), SharedModule],
 })
 export class IndustryModule {}
