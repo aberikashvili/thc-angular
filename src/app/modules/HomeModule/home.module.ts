e2f04dd39d249e902da4f3c45229dde1f2912c60
ucubabu <ucunashviligio@gmail.com>
2023-05-21 15:22:22 +0400

    finished

--- src/app/modules/HomeModule/home.module.ts
+++ src/app/modules/HomeModule/home.module.ts
@@ -3,7 +3,7 @@ import { HomeComponent } from './home.component';
 import { RouterModule, Routes } from '@angular/router';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { DistributioncenterComponent } from './components/distributioncenter/distributioncenter.component';
-import { WhoWeAreComponent } from './who-we-are/who-we-are.component';
+
 
 const routes: Routes = [
   {
@@ -13,7 +13,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  declarations: [HomeComponent, DistributioncenterComponent, WhoWeAreComponent],
+  declarations: [HomeComponent, DistributioncenterComponent, ],
   imports: [RouterModule.forChild(routes), SharedModule],
 })
 export class HomeModule {}
