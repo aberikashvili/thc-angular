a787caa984e43f21b0a032088a16f900e6291b06
ucubabu <ucunashviligio@gmail.com>
2023-05-27 17:37:48 +0400

    some adds

--- src/app/modules/brand-details-page/brand-details-page.module.ts
+++ src/app/modules/brand-details-page/brand-details-page.module.ts
@@ -4,12 +4,12 @@ import { BrandDetailsPageComponent } from './brand-detail-page.component';
 import { RouterTestingModule } from '@angular/router/testing';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { RouterModule, Routes } from '@angular/router';
-import { BranddetailsComponent } from './components/branddetails/branddetails.component';
+// import { BranddetailsComponent } from '../who-we-are/branddetails/branddetails.component';
 
 const routes: Routes = [{ path: '', component: BrandDetailsPageComponent }];
 
 @NgModule({
-  declarations: [BrandDetailsPageComponent, BranddetailsComponent],
+  declarations: [BrandDetailsPageComponent, ],
   imports: [RouterModule.forChild(routes), SharedModule],
 })
 export class BrandDetailPageModule {}
