c5cdfdc7b869046ce1351f09f19c3c6ad40affd1
ucubabu <ucunashviligio@gmail.com>
2023-05-27 17:58:22 +0400

    some adds

--- src/app/modules/brand-details-page/brand-details-page.module.ts
+++ src/app/modules/brand-details-page/brand-details-page.module.ts
@@ -1,15 +1,17 @@
+
+import { BrandDetailsDescriptionSectionComponent } from './components/brand-details-description-section/brand-details-description-section.component'
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BrandDetailsPageComponent } from './brand-detail-page.component';
 import { RouterTestingModule } from '@angular/router/testing';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { RouterModule, Routes } from '@angular/router';
-// import { BranddetailsComponent } from '../who-we-are/branddetails/branddetails.component';
 
-const routes: Routes = [{ path: '', component: BrandDetailsPageComponent }];
+const routes: Routes = [{ path: '', 
+component: BrandDetailsPageComponent }];
 
 @NgModule({
-  declarations: [BrandDetailsPageComponent, ],
+  declarations: [BrandDetailsPageComponent, BrandDetailsDescriptionSectionComponent,  ],
   imports: [RouterModule.forChild(routes), SharedModule],
 })
 export class BrandDetailPageModule {}
---
+++ src/app/modules/brand-details-page/components/brand-details-description-section/brand-details-description-section.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-brand-details-description-section',
+  templateUrl: './brand-details-description-section.component.html',
+  styleUrls: ['./brand-details-description-section.component.scss']
+})
+export class BrandDetailsDescriptionSectionComponent {
+
+}
