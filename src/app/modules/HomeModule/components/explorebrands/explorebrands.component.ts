e95499277009067ee3606d02411b3336ee3aad15
ucubabu <ucunashviligio@gmail.com>
2023-06-04 18:45:46 +0400

    Update explorebrands.component.ts

--- src/app/modules/HomeModule/components/explorebrands/explorebrands.component.ts
+++ src/app/modules/HomeModule/components/explorebrands/explorebrands.component.ts
@@ -1,31 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component,} from '@angular/core';
 
 @Component({
   selector: 'app-explorebrands',
   templateUrl: './explorebrands.component.html',
   styleUrls: ['./explorebrands.component.scss'],
 })
-export class ExplorebrandsComponent implements OnInit {
-  ngOnInit(): void {
-    this.filterData();
-  }
-  categoryData: any = []
-   shinkhoferArray:any = [];
-
+export class ExplorebrandsComponent {
  
-
-filterData(){
-  this.shinkhoferArray = [
-    { logo: '/assets/images/schirnhoferlogo.png', showOnMobileVersion: true },
-    { logo: '/assets/images/schirnhoferlogo.png', showOnMobileVersion: true },
-    { logo: '/assets/images/schirnhoferlogo.png', showOnMobileVersion: true },
-    { logo: '/assets/images/schirnhoferlogo.png', showOnMobileVersion: true, detailUrl: 'Details' },
-    { logo: '/assets/images/schirnhoferlogo.png', showOnMobileVersion: true },
-    { logo: '/assets/images/schirnhoferlogo.png', showOnMobileVersion: true },
-    { logo: '/assets/images/schirnhoferlogo.png', showOnMobileVersion: false },
-    { logo: '/assets/images/schirnhoferlogo.png', showOnMobileVersion: false },
-  ];
-}
 }
 
 
