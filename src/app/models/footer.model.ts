cdea981561b3ab7fb00a1fd08997242b085fc503
koksoni <47037656+koksoni@users.noreply.github.com>
2023-06-06 02:15:39 +0400

    mockup

---
+++ src/app/models/footer.model.ts
@@ -0,0 +1,23 @@
+export class Footer{
+    constructor(
+    public company: string,
+    public quicklinks: string,
+    public socialnetwork: string,
+    public contact: string,
+    public whoweare: string,
+    public industry: string,
+    public ourbrands: string,
+    public media: string,
+    public news: string,
+    public facebook: string,
+    public linkedin: string,
+    public youtube: string,
+    public description:string,
+    public caption:string,
+    public phonecaption: string,
+    public emailcaption: string,
+    public rightscaption: string,
+    public termscaption: string,
+    public privacycaption: string
+    ) {}
+}
