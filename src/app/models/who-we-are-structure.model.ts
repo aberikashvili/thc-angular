a17ec913f360c65c3697b9d4586b79e6ad8dc53c
ucubabu <ucunashviligio@gmail.com>
2023-06-14 21:43:12 +0400

    33-who-we-are-ის-structure-of-the-company-sectionmockup

---
+++ src/app/models/who-we-are-structure.model.ts
@@ -0,0 +1,19 @@
+export class WhoWeAreStructure {
+  constructor(
+    public title: string,
+    public holdinTittle :string,
+    public holdinDescription : string,
+    public foodTittle : string,
+    public foodDescription : string,
+    public toyTittle :string,
+    public toyDescription : string,
+    public supermarketTittle : string,
+    public supermarketDescription : string,
+    public constructionTittle : string,
+    public constructionDescription : string ,
+    public productionTittle : string,
+    public productionDescription :string,
+    
+  
+  ) {}
+}
--- src/app/modules/who-we-are/components/who-we-are-structure/who-we-are-structure.component.ts
+++ src/app/modules/who-we-are/components/who-we-are-structure/who-we-are-structure.component.ts
@@ -1,10 +1,46 @@
 import { Component } from '@angular/core';
+import { DataService } from 'src/app/core/services/data.service';
 
 @Component({
   selector: 'app-who-we-are-structure',
   templateUrl: './who-we-are-structure.component.html',
-  styleUrls: ['./who-we-are-structure.component.scss']
+  styleUrls: ['./who-we-are-structure.component.scss'],
 })
 export class WHOWEAREStructureComponent {
+  title!: string;
+  holdinTittle!: string;
+  holdinDescription!: string;
+  foodTittle!: string;
+  foodDescription!: string;
+  toyTittle!: string;
+  toyDescription!: string;
+  supermarketTittle!: string;
+  supermarketDescription!: string;
+  constructionTittle!: string;
+  constructionDescription!: string;
+  productionTittle!: string;
+  productionDescription!: string;
 
+  constructor(private _dataservice: DataService) {}
+  ngOnInit(){
+    this._dataservice.getWhoWeAreStructure().subscribe((data)=>{
+      this.title =data.title ;
+      this.holdinTittle= data.holdinTittle;
+      this.holdinDescription =data.holdinDescription;
+      this.foodTittle =data.foodTittle;
+      this.foodDescription =data.foodDescription;
+      this.toyTittle=data.toyTittle;
+      this.toyDescription=data.toyDescription;
+      this.supermarketTittle=data.supermarketTittle;
+      this.supermarketDescription=data.supermarketTittle;
+      this.supermarketDescription=data.supermarketDescription;
+      this.constructionTittle=data.constructionTittle;
+      this.constructionDescription=data.constructionDescription;
+      this.productionTittle=data.constructionTittle;
+      this.constructionDescription=data.constructionDescription;
+    })
+      
+
+    
+  }
 }
