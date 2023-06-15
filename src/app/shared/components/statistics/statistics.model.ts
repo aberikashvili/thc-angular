88a5f1cc89cb501268f456903043f7b956e2e76b
tengo7777 <108832273+tengo7777@users.noreply.github.com>
2023-06-15 03:32:20 -0700

    update

---
+++ src/app/shared/components/statistics/statistics.model.ts
@@ -0,0 +1,8 @@
+export class Statistics {
+  constructor(
+    public sectionTitle: string,
+    public title: string,
+    public description: string,
+    public statistics: { title: string; description: string }[]
+  ) {}
+}
