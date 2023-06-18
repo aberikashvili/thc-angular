ec712ae74b4c4c0540f5d21b9cf4615b03cb69ab
koksoni <47037656+koksoni@users.noreply.github.com>
2023-06-19 02:14:55 +0400

    .

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,7 +5,7 @@ import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeroSectionComponent } from './components/hero-section/hero-section.component';
 import { StatisticsComponent } from '../modules/HomeModule/components/statistics/statistics.component';
-import { StructureOfTheCompanyComponent } from '../modules/HomeModule/components/structure-of-the-company/structure-of-the-company.component';
+import { StructureOfTheCompanyComponent } from './components/structure-of-the-company/structure-of-the-company.component';
 import { RouterModule } from '@angular/router';
 import { OurBrandsComponent } from '../modules/our-brands/our-brands.component';
 import { GalleryComponent } from './components/gallery/gallery.component';
