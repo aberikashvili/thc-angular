b206e2c641c5f4332171249714d75f051854a51b
ucubabu <ucunashviligio@gmail.com>
2023-06-23 20:02:38 +0400

    added ChangeDetectorRe

--- src/app/models/who-we-are-slider.entry.ts
+++ src/app/models/who-we-are-slider.entry.ts
@@ -1,6 +1,6 @@
 export class WhoWeAreSliderEntry {
   constructor(
-    public title: string,
+    public tittle: string,
     public subTitle: string,
     public description: string,
     public images: string[]
--- src/app/modules/who-we-are/components/who-we-are-media/who-we-are-media.component.ts
+++ src/app/modules/who-we-are/components/who-we-are-media/who-we-are-media.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component } from '@angular/core';
 import { DataService } from 'src/app/core/services/data.service';
 
 @Component({
@@ -6,18 +6,19 @@ import { DataService } from 'src/app/core/services/data.service';
   templateUrl: './who-we-are-media.component.html',
   styleUrls: ['./who-we-are-media.component.scss'],
 })
-export class WhoWeAreMediaComponent  {
+export class WhoWeAreMediaComponent {
   tittle!: string;
   subtittle!: string;
   images!: string[];
 
-  constructor(private _datasevice: DataService) {}
+  constructor(private _datasevice: DataService, private _cdr: ChangeDetectorRef) {}
 
   ngOnInit() {
     this._datasevice.getWhoWeAreMedia().subscribe((data) => {
       this.tittle = data.tittle;
       this.subtittle = data.subtittle;
       this.images = data.images;
+      this._cdr.markForCheck();
     });
   }
 }
