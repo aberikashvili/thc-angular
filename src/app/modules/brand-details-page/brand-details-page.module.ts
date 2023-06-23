b1486b5e83d2a5479b5e5e5d9402f93064e1afba
ucubabu <ucunashviligio@gmail.com>
2023-06-23 13:44:09 +0400

    .

--- src/app/modules/brand-details-page/brand-details-page.module.ts
+++ src/app/modules/brand-details-page/brand-details-page.module.ts
@@ -1,17 +1,15 @@
 
 import { BrandDetailsDescriptionSectionComponent } from './components/brand-details-description-section/brand-details-description-section.component'
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { BrandDetailsPageComponent } from './brand-detail-page.component';
-import { RouterTestingModule } from '@angular/router/testing';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { RouterModule, Routes } from '@angular/router';
-
+import { WhoWeAreMediaComponent } from '../who-we-are/components/who-we-are-media/who-we-are-media.component';
 const routes: Routes = [{ path: '', 
 component: BrandDetailsPageComponent }];
 
 @NgModule({
-  declarations: [BrandDetailsPageComponent, BrandDetailsDescriptionSectionComponent,  ],
+  declarations: [BrandDetailsPageComponent, BrandDetailsDescriptionSectionComponent,WhoWeAreMediaComponent, ],
   imports: [RouterModule.forChild(routes), SharedModule],
 })
 export class BrandDetailPageModule {}
