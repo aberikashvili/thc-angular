15a48c4179749f6f8a9f23ca1cdcfbcc526a73bb
tengo7777 <108832273+tengo7777@users.noreply.github.com>
2023-06-26 02:28:29 -0700

    finished

--- src/app/modules/contact-us/contact-us.component.ts
+++ src/app/modules/contact-us/contact-us.component.ts
@@ -8,5 +8,34 @@ import { FormControl, Validators } from '@angular/forms';
   encapsulation: ViewEncapsulation.None,
 })
 export class ContactUsComponent {
-  email = new FormControl('', [Validators.email]);
+  email = new FormControl('', [Validators.required, Validators.email]);
+  firstName = new FormControl('', [Validators.required]);
+  lastName = new FormControl('', [Validators.required]);
+  companyName = new FormControl('', [Validators.required]);
+  textArea = new FormControl('', [Validators.required]);
+
+  onSubmit() {
+    const data = {
+      email: this.email,
+      firstName: this.firstName,
+      lastName: this.lastName,
+      companyName: this.companyName,
+      textArea: this.textArea,
+    };
+    console.log(data);
+  }
+
+  checkInputs() {
+    if (
+      this.email.invalid ||
+      this.firstName.invalid ||
+      this.lastName.invalid ||
+      this.companyName.invalid ||
+      this.textArea.invalid
+    ) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
